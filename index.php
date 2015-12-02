26da01d59515626c1c485a43bbc4d7d552f3345f
Theo Godard <theo.godard@gmail.com>
2015-12-02 01:01:32 +0000

    Start again with a basic SlimPHP based application

--- index.php
+++ index.php
@@ -1,138 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+$app = new \Slim\Slim();
 
-    <title>Run Git Hooks</title>
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
 
-    <!-- Bootstrap Core CSS -->
-    <link href="./bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+$app->run();
 
-    <!-- MetisMenu CSS -->
-    <link href="./bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Timeline CSS -->
-    <link href="./dist/css/timeline.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="./dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Morris Charts CSS -->
-    <link href="./bower_components/morrisjs/morris.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="./bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Run Git Hooks</a>
-            </div>
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    
-                    <?php
-                    
-                    ?>
-                    
-                    <ul class="nav" id="side-menu">
-                        <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Script 1</a>
-                        </li>
-                        <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Script 2</a>
-                        </li>
-                        <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Script 3</a>
-                        </li>
-                        <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Script 4</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Current Script</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <i class="fa fa-terminal fa-fw"></i> Console Output
-                        </div>
-                        <div class="panel-body">
-                            
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-lg-4">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <i class="fa fa-wrench fa-fw"></i> Options
-                        </div>
-                        <div class="panel-body">
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- jQuery -->
-    <script src="./bower_components/jquery/dist/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="./bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="./bower_components/raphael/raphael-min.js"></script>
-    <script src="./bower_components/morrisjs/morris.min.js"></script>
-    <script src="./js/morris-data.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="./dist/js/sb-admin-2.js"></script>
-    
-    <!-- Core JavaScript -->
-    <script src="./js/list_scripts.js"></script>
-    <script src="./js/run_script.js"></script>
-
-</body>
-
-</html>
+?>
--- js/list_scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-	var script_dir = '../shell_scripts';
-
-	$("#side-menu").html('');
-	
-	
-});
